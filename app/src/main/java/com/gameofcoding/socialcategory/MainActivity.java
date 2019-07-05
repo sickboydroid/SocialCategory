716966dce27c7768cf580d589deeb3908cd7fb04
Game-Of-Coding <gameofcoding@gmail.com>
2019-07-05 19:09:49 +0530

    First Update

---
+++ app/src/main/java/com/gameofcoding/socialcategory/MainActivity.java
@@ -0,0 +1,21 @@
+package com.gameofcoding.socialcategory;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import android.widget.Toast;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_test);
+    }
+}
